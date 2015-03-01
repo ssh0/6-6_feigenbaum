8acab6c9e13a06e4afcc4dde1214939e49d2bd66
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-03-01 18:32:42 +0900

    modify

--- 6-6_feigenbaum-a.py
+++ 6-6_feigenbaum-a.py
@@ -9,7 +9,7 @@
 
 import matplotlib.pylab as plt
 from Tkinter import *
-import numpy
+import numpy as np
 
 b = [0.750000, 0.862372, 0.886023, 0.891102,
      0.892190, 0.892423, 0.892473, 0.892484]
@@ -19,21 +19,18 @@ b = [0.750000, 0.862372, 0.886023, 0.891102,
 def delta(k):
     return (b[k - 1] - b[k - 2]) / (b[k] - b[k - 1])
 
-plt.gca().set_xlim(1.5, len(b) - 0.5)
-plt.xlabel(r'$k$')
-plt.ylabel(r'$\delta_{k}$')
-plt.title('Feigenbaum constant')
-sum_delta_k = 0
+b_index = range(2, len(b))
+delta_k = np.array(map(delta, b_index))
+ave_delta_k = np.average(delta_k)
 
-for k in range(2, len(b)):
-    plt.scatter(k, delta(k), color='r', s=8, marker='o')
-    sum_delta_k = sum_delta_k + delta(k)
-ave_delta_k = sum_delta_k / 6
-
+plt.scatter(b_index, delta_k, color='r', s=8, marker='o')
 plt.plot([2, len(b) - 1], [ave_delta_k] * 2,
          label=r'$\mathrm{average}\ \mathrm{of}\ \delta_{k}\ :\ \delta \ =\ $'
          + str(ave_delta_k)
          )
-
-plt.legend(loc="best")
+plt.gca().set_xlim(1.5, len(b) - 0.5)
+plt.xlabel(r'$k$')
+plt.ylabel(r'$\delta_{k}$')
+plt.title('Feigenbaum constant')
+plt.legend(loc='best')
 plt.show()
